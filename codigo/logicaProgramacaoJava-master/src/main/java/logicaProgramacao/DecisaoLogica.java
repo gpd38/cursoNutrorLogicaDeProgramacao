4a6c1e7c75785fbf28e5b1f0dd641f2c048f3816
Gustavo Dias <gustavodias2@gmail.com>
2021-05-10 23:34:37 -0300

    Atualizando classe DecisaoLogica.java

--- codigo/logicaProgramacaoJava-master/src/main/java/logicaProgramacao/DecisaoLogica.java
+++ codigo/logicaProgramacaoJava-master/src/main/java/logicaProgramacao/DecisaoLogica.java
@@ -2,7 +2,7 @@ package logicaProgramacao;
 
 public class DecisaoLogica {
 
-	// 19. Entrar via teclado, com dois valores distintos. Exibir o maior deles.
+	/** 19. Entrar via teclado, com dois valores distintos. Exibir o maior deles. */
 	public int getMaior(int val1, int val2) {
 		if (val1 > val2) {
 			return val1;
@@ -10,10 +10,12 @@ public class DecisaoLogica {
 		return val2;
 	}
 
-	// 20. Entrar via teclado, com dois valores distintos. Exibir o menor deles.
+	/** 20. Entrar via teclado, com dois valores distintos. Exibir o menor deles. */
 
-	// 21. Entrar com dois valores quaisquer. Exibir o maior deles, se existir, caso
-	// contrário, enviar mensagem avisando que os números são idênticos.
+	/**
+	 * 21. Entrar com dois valores quaisquer. Exibir o maior deles, se existir, caso
+	 * contrário, enviar mensagem avisando que os números são idênticos.
+	 */
 	public Object getMaiorIgual(int val1, int val2) {
 		if (val1 > val2) {
 			return val1;
@@ -23,19 +25,25 @@ public class DecisaoLogica {
 		return "numeros identicos";
 	}
 
-	// 22. Calcular e exibir a área de um retângulo, a partir dos valores da base e
-	// altura que serão digitados. Se a área for maior que 100, exibir a mensagem
-	// “Terreno grande”.
+	/**
+	 * 22. Calcular e exibir a área de um retângulo, a partir dos valores da base e
+	 * altura que serão digitados. Se a área for maior que 100, exibir a mensagem
+	 * “Terreno grande”.
+	 */
 
-	// 23. Calcular e exibir a área de um retângulo, a partir dos valores da base e
-	// altura que serão digitados. Se a área for maior que 100, exibir a mensagem
-	// “Terreno grande”, caso contrário, exibir a mensagem “Terreno pequeno”.
+	/**
+	 * 23. Calcular e exibir a área de um retângulo, a partir dos valores da base e
+	 * altura que serão digitados. Se a área for maior que 100, exibir a mensagem
+	 * “Terreno grande”, caso contrário, exibir a mensagem “Terreno pequeno”.
+	 */
 
-	// 24. Entrar via teclado com três valores distintos. Exibir o maior deles.
+	/** 24. Entrar via teclado com três valores distintos. Exibir o maior deles. */
 
-	// 25. Entrar com o peso e a altura de uma determinada pessoa. Após a digitação,
-	// exibir se esta pessoa está ou não com seu peso ideal. Veja tabela da relação
-	// peso/altura².
+	/**
+	 * 25. Entrar com o peso e a altura de uma determinada pessoa. Após a digitação,
+	 * exibir se esta pessoa está ou não com seu peso ideal. Veja tabela da relação
+	 * peso/altura².
+	 */
 	public String getPesoIdeal(double peso, double altura) {
 		double r = peso / (altura * altura);
 		if (r < 20) {
@@ -47,12 +55,14 @@ public class DecisaoLogica {
 
 	}
 
-	// 26. A partir de três valores que serão digitados, verificar se formam ou não
-	// um triângulo. Em caso positivo, exibir sua classificação: “Isósceles,
-	// escaleno ou eqüilátero”. Um triângulo escaleno possui todos os lados
-	// diferentes, o isósceles, dois lados iguais e o eqüilátero, todos os lados
-	// iguais. Para existir triângulo é necessário que a soma de dois lados
-	// quaisquer seja maior que o outro, isto, para os três lados.
+	/**
+	 * 26. A partir de três valores que serão digitados, verificar se formam ou não
+	 * um triângulo. Em caso positivo, exibir sua classificação: “Isósceles,
+	 * escaleno ou eqüilátero”. Um triângulo escaleno possui todos os lados
+	 * diferentes, o isósceles, dois lados iguais e o eqüilátero, todos os lados
+	 * iguais. Para existir triângulo é necessário que a soma de dois lados
+	 * quaisquer seja maior que o outro, isto, para os três lados.
+	 */
 	public String getTriangulo(int a, int b, int c) {
 
 		if (a + b > c || a + c > b || b + c > a) {
@@ -68,50 +78,59 @@ public class DecisaoLogica {
 
 	}
 
-	// 27. Verificar se três valores quaisquer (A,B, C) que serão digitados formam
-	// ou não um triângulo retângulo. Lembre-se que o quadrado da hipotenusa é igual
-	// a soma dos quadrados dos catetos.
+	/**
+	 * 27. Verificar se três valores quaisquer (A,B, C) que serão digitados formam
+	 * ou não um triângulo retângulo. Lembre-se que o quadrado da hipotenusa é igual
+	 * a soma dos quadrados dos catetos.
+	 */
 
-	// 28. Entrar com o peso, o sexo e a altura de uma determinada pessoa. Após a
-	// digitação, exibir se esta pessoa está ou não com seu peso ideal. Veja tabela
-	// da relação peso/altura².
-	// =======================================
-	// Peso/altura² (R) - Femininos Mensagem
-	// R < 19 Abaixo do peso
-	// 19 <= R < 24 Peso ideal
-	// R >= 24 Acima do peso
-	// =======================================
-	// peso/altura² (R) - Masculinos Mensagem
-	// R < 20 Abaixo do peso
-	// 20 <= R < 25 Peso ideal
-	// R >= 25 Acima do peso
+	/**
+	 * 28. Entrar com o peso, o sexo e a altura de uma determinada pessoa. Após a
+	 * digitação, exibir se esta pessoa está ou não com seu peso ideal. Veja tabela
+	 * da relação peso/altura².
+	 * 
+	 * ======================================= 
+	 * Peso/altura² (R) - Femininos Mensagem
+	 * R < 19 Abaixo do peso 
+	 * 19 <= R < 24 Peso ideal 
+	 * R >= 24 Acima do peso
+	 * ======================================= 
+	 * peso/altura² (R) - Masculinos
+	 * Mensagem R < 20 Abaixo do peso 
+	 * 20 <= R < 25 
+	 * Peso ideal 
+	 * R >= 25 Acima do peso
+	 */
 
-	// 29. A partir dos valores da aceleração (a em m/s²), da velocidade inicial (v0
-	// em m/s) e do tempo de percurso (t em s). Calcular e exibir a velocidade final
-	// de automóvel em km/h. 
-	// Fórmula para o cálculo da velocidade em m/s:  V = v0 + a. t
-	// Exibir mensagem de acordo com a tabela:
-	// ======================================= 
-	// Velocidade em Km/h (V) Mensagem
-	// V <= 40 Veículo muito lento
-	// 40 < V <= 60 Velocidade permitida
-	// 60 < V <= 80 Velocidade de cruzeiro
-	// 80 < V <= 120 Veículo rápido
-	// V > 120 Veículo muito rápido
+	/**
+	 * 29. A partir dos valores da aceleração (a em m/s²), da velocidade inicial (v0
+	 * em m/s) e do tempo de percurso (t em s). Calcular e exibir a velocidade final
+	 * de automóvel em km/h. Fórmula para o cálculo da velocidade em m/s: V = v0 +
+	 * a. t Exibir mensagem de acordo com a tabela: 
+	 * 
+	 * ======================================= 
+	 * Velocidade em Km/h (V) Mensagem 
+	 * V <= 40 Veículo muito lento 
+	 * 40 < V <= 60 Velocidade permitida 
+	 * 60 < V <= 80 Velocidade de cruzeiro 
+	 * 80 < V <= 120 Veículo rápido 
+	 * V > 120 Veículo muito rápido
+	 */
 
-	// 30. Uma escola com cursos em regime semestral, realiza duas avaliações
+	/** 30. Uma escola com cursos em regime semestral, realiza duas avaliações
 	// durante o semestre e calcula a média do aluno, da seguinte maneira:
 	// MEDIA = (P1 + 2P2) / 3
 	// Fazer um programa para entrar via teclado com os valores das notas (P1 e P2)
 	// e calcular a média. Exibir a situação final do aluno (“Aprovado ou
-	// Reprovado”), sabendo que a média de aprovação é igual a cinco.
+	// Reprovado”), sabendo que a média de aprovação é igual a cinco.*/
 
-	// 31. Uma escola com cursos em regime semestral realiza duas avaliações durante
-	// o semestre e calcula a média do aluno, da seguinte maneira:
-	// MEDIA = (P1 +2P2) / 3
-	// Fazer um programa para entrar via teclado com o valor da primeira nota (P1) e
-	// o programa deverá calcular e exibir quanto o aluno precisa tirar na segunda
-	// nota (P2) para ser aprovado, sabendo que a média de aprovação é igual a
-	// cinco.
+	/**
+	 * 31. Uma escola com cursos em regime semestral realiza duas avaliações durante
+	 * o semestre e calcula a média do aluno, da seguinte maneira:  MEDIA = (P1 +2P2) / 3.
+	 * Fazer um programa para entrar via teclado com o valor da primeira nota
+	 * (P1) e o programa deverá calcular e exibir quanto o aluno precisa tirar na
+	 * segunda nota (P2) para ser aprovado, sabendo que a média de aprovação é igual
+	 * a cinco.
+	 */
 
 }
