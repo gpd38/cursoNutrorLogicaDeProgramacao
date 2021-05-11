bce7dfdabe37bde5d96c7fce401265b2128e9f9e
Gustavo Dias <gustavodias2@gmail.com>
2021-05-10 23:38:06 -0300

    Atualizando classe de teste EstruturaRepeticao.java

--- codigo/logicaProgramacaoJava-master/src/test/java/logicaProgramacao/EstruturaRepeticaoTest.java
+++ codigo/logicaProgramacaoJava-master/src/test/java/logicaProgramacao/EstruturaRepeticaoTest.java
@@ -8,15 +8,56 @@ import org.junit.Test;
 
 public class EstruturaRepeticaoTest {
 	EstruturaRepeticao estruturaRepeticao = new EstruturaRepeticao();
+
+	/** Teste 32 */
+
+	/** Teste 33 */
+
+	/** Teste 34 */
+
+	/** Teste 35 */
 	@Test
 	public void exibeTabuadaCinco() {
 		estruturaRepeticao.getTabuadaCinco();
 	}
-	
+
+	/** Teste 36 */
+
+	/** Teste 37 */
+
+	/** Teste 38 */
+
+	/** Teste 39 */
 	@Test
 	public void exibeSomaInteirosUmACem() {
 		estruturaRepeticao.somaNumerosPositivosUmACem();
 	}
+
+	/** Teste 40 */
+
+	/** Teste 41 */
+
+	/** Teste 42 */
+
+	/** Teste 43 */
+
+	/** Teste 44 */
+
+	/** Teste 45 */
+
+	/** Teste 46 */
+
+	/** Teste 47 */
+
+	/** Teste 48 */
+
+	/** Teste 49 */
+
+	/** Teste 50 */
+
+	/** Teste 51 */
+
+	/** Teste 52 */
 	@Test
 	public void exibeValorInverso() {
 		List<Integer> listaNumeros = new ArrayList<Integer>();
@@ -32,19 +73,26 @@ public class EstruturaRepeticaoTest {
 		listaNumeros.add(28);
 		estruturaRepeticao.exibirValorOrdemInversa(listaNumeros);
 	}
-	
+
+	/** Teste 53 */
+
+	/** Teste 54 */
+
+	/** Teste 55 */
 	@Test
 	public void ExibeConstanteMultiplicativa() {
-		int[] vetor = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
-				12, 13, 14, 15, 16, 17, 18, 19, 20};
+		int[] vetor = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
 		int constante = 5;
 		estruturaRepeticao.constanteMultiplicativa(vetor, constante);
-		
+
 	}
-	
+
+	/** Teste 56 */
+
+	/** Teste 57 */
 	@Test
 	public void exibeListaNomeIdade() {
-		List<LinkedHashMap<String, Object>> listMap = new ArrayList<LinkedHashMap<String,Object>>();
+		List<LinkedHashMap<String, Object>> listMap = new ArrayList<LinkedHashMap<String, Object>>();
 		LinkedHashMap<String, Object> map1 = new LinkedHashMap<>();
 		LinkedHashMap<String, Object> map2 = new LinkedHashMap<>();
 		LinkedHashMap<String, Object> map3 = new LinkedHashMap<>();
@@ -65,7 +113,52 @@ public class EstruturaRepeticaoTest {
 		listMap.add(map3);
 		listMap.add(map4);
 		listMap.add(map5);
-							
+
 		estruturaRepeticao.exibeDados(listMap);
 	}
+	/** Teste 58 */
+
+	/** Teste 59 */
+
+	/** Teste 60 */
+
+	/** Teste 61 */
+
+	/** Teste 62 */
+
+	/** Teste 63 */
+
+	/** Teste 64 */
+
+	/** Teste 65 */
+
+	/** Teste 66 */
+
+	/** Teste 67 */
+
+	/** Teste 68 */
+
+	/** Teste 69 */
+
+	/** Teste 70 */
+
+	/** Teste 71 */
+
+	/** Teste 72 */
+
+	/** Teste 73 */
+
+	/** Teste 74 */
+
+	/** Teste 75 */
+
+	/** Teste 76 */
+
+	/** Teste 77 */
+
+	/** Teste 78 */
+
+	/** Teste 79 */
+
+	/** Teste 80 */
 }
